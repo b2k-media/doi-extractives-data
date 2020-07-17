5ca7aaeb3f919c1adaa7b06c2ec7ea3e2b29f89d
Artjom Sajatz-Davydov <a.sajatz-davydov@b2k-media.de>
2020-07-17 12:19:53 +0200

    /daten/zahlungsabgleich

--- js/pages/barGraph.js
+++ js/pages/barGraph.js
@@ -195,7 +195,7 @@ $(document).ready(function(){
     plot2b = $.jqplot(chart, data, plot_options);
     if(jsonUnit != null)
       $(".jqplot-xaxis-tick").last().text(jsonUnit);
-    else if (chartTitle == "Abdeckung der Sektoren*" || chartTitle == "Abdeckung der Sektoren* EN")
+    else if (chartTitle == "Abdeckung der Sektoren durch die an D-EITI teilnehmenden Unternehmen" || chartTitle == "Abdeckung der Sektoren EN")
       $(".jqplot-xaxis-tick").last().text('100%');
     else
       $(".jqplot-xaxis-tick").last().text('Mio. €');
